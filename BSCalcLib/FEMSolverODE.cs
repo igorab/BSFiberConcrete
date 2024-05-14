8591718ef1883186e076fa9cb8e3d53ced25ea3e
Balakirev Igor <igor.balakirev@mail.ru>
2024-05-14 16:22:30 +0300

    Dirichlet

--- BSCalcLib/FEMSolverODE.cs
+++ BSCalcLib/FEMSolverODE.cs
@@ -14,9 +14,16 @@ namespace BSCalcLib
 
     public class FEMSolverODE
     {
-        public List<double> X { set { m_X = value.ToArray(); } }
+        public List<double> X 
+        { 
+            set 
+            { 
+                m_X = value.ToArray(); 
+                NPoints = m_X.Length;
+                NElem = NPoints - 1;
+            } 
+        }
   
-
         // total number of nodes
         private static int NPoints;
         // total number of elements
@@ -24,58 +31,63 @@ namespace BSCalcLib
         // x-coordinate of node I
         private double[] m_X = new double[NPoints];
 
+        private const int ElPoints = 2;
+
         // the M-N-th element of the element X matrix, M and N being node identifiers
-        double[,] STE = new double[NPoints, NPoints];
+        double[,] STE = new double[ElPoints, ElPoints];
 
         // the I-J element of the assembled system K matrix
         double[,] ST = new double[NPoints, NPoints];
 
         //  Right-hand side matrix in the system matrix equasion
-        double[,] RHS = new double[NPoints, NPoints];
+        double[] RHS = new double[NPoints];
 
 
         static FEMSolverODE()
         {
-            NPoints = 3;
-            NElem = 2;
+            NPoints = 7;
+            NElem = NPoints-1;
         }
 
         //The system K matrix and the Right-hand side matrix are initilized to Zero
-        private void InitSTRHS()
+        private void InitToZeros()
         {
             for (int i = 0; i < NPoints; i++) 
             {
+                RHS[i] = 0;
+
                 for (int j = 0; j<NPoints; j++)
                 {
-                    ST[i, j] = 0;
-                    RHS[i, j] = 0;
+                    ST[i, j] = 0;                    
                 }
             }
         }
 
         // the element K Matrices atr obtained and assembled for all elements and the system k matrix is obtained
-        private void InitSTEST()
+        private void InitSTMatrix()
         {
             for (int i = 0; i < NElem; i++)
             {
                 double coef = m_X[i + 1] - m_X[i];
 
-                STE[1,1] = coef/3.0 + 1.0/coef;
-                STE[1,2] = coef/6.0 - 1.0 / coef;
-                STE[2, 2] = STE[1, 1];
-                STE[2, 1] = STE[1, 2];
-
-                ST[i, i] += STE[1, 1];
-                ST[i, i+1] += STE[1, 2];
-                ST[i+1, i] += STE[2, 1];
-                ST[i+1, i+1] += STE[2, 2];
+                STE[0,0] = coef/3.0 + 1.0/coef;
+                STE[0,1] = coef/6.0 - 1.0 / coef;
+                STE[1, 0] = STE[0, 1];
+                STE[1,1] = STE[1, 1];
+                
+                ST[i, i] += STE[0, 0];
+                ST[i, i+1] += STE[0, 1];
+                ST[i+1, i] += STE[1, 0];
+                ST[i+1, i+1] += STE[1, 1];
             }
         }
 
         // the Dirichlet boundary conditions are inserted in the right-hand side matrix and the system k matrix is corrected
         private void InitBC()
         {
-            for (int i=0; i < NPoints; i ++)
+            List<int> bounds = new List<int>() {0, NPoints-1 } ;
+
+            foreach (int i in bounds) 
             {
                 for (int j = 0; j < NPoints; j++)
                 {
@@ -83,8 +95,18 @@ namespace BSCalcLib
                 }
 
                 ST[i, i] = 1.0;
-                RHS[i, 1] = Math.Exp(m_X[i]);
+                RHS[i] = Math.Exp(m_X[i]);
             }
+            
+        }
+
+        public void Run()
+        {
+            InitToZeros();
+
+            InitSTMatrix();
+
+            InitBC();
         }
 
         public static void RunFromCode()
@@ -96,13 +118,6 @@ namespace BSCalcLib
             solverODE.Run();
         }
 
-        public void Run()
-        {
-            InitSTRHS();
-
-            InitSTEST();
-
-            InitBC();
-        }
+      
     }
 }
