95bcb7fb55237e1b483d7d9b14c935384fb422b6
vlfilrab <vlfilrab14@gmail.com>
2024-06-13 12:02:28 +0300

    [BSBeamCalculator] незначительные измнения в оформлении кода: Переменнование классов

--- BSBeamCalculator/model/BeamDiagram.cs
+++ BSBeamCalculator/model/BeamDiagram.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Schema;
 
-namespace BSBeamCalculator.model
+namespace BSBeamCalculator
 {
     public class BeamDiagram
     {
