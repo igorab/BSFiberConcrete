daa5e29313307bcdfc81f37f6327fffe7fb7630e
vlfilrab <vlfilrab14@gmail.com>
2024-06-28 12:11:07 +0300

    [BSFiberCalculator] удаление неиспользуемой формы.

--- BSBeamCalculator/BeamCalculatorRun.cs
+++ BSBeamCalculator/BeamCalculatorRun.cs
@@ -18,7 +18,7 @@ namespace BSBeamCalculator
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new BeamCalculatorForm1());
+                Application.Run(new BeamCalculatorForm());
             }
         }
 
